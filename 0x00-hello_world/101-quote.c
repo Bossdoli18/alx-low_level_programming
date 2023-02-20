b4f6e8bfa0912eda73db299b69dceaec26d2aeda
Bossdoli18 <oladeleife18@gmail.com>
2023-02-20 03:43:59 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints out the last part of a quote in the standard error.
+ * main - Entry point
  *
  * Return: 1 if success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
