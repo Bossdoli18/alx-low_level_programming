819171cbb32193503302ddf1e0676bae8c249996
Bossdoli18 <oladeleife18@gmail.com>
2023-02-22 08:11:06 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-#include <unistd.h>
-
 /**
-* _putchar 
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-* */
-
-int _putchar(char c)
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
+	char *sh = "_putchar";
 
-         return (write(1, &c, 1));
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
 
+	return (0);
 }
 
