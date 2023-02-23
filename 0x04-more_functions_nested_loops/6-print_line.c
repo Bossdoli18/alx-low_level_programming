5f6f62c4f62a26ab773be0a18e174298b7ee1549
Bossdoli18 <oladeleife18@gmail.com>
2023-02-23 12:34:46 -0800

    print square

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,29 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_line - function declaration
+ * Description: print dashes n times
+ * @n: number of dashes to print
+ * Return: has no return values
  */
 
-int main(void)
+void print_line(int n);
+
+/**
+ * print_line - function definition
+ * Description: print dashes as many as n times
+ * @n: number of dashes
+ * Return: has none
+ */
+
+void print_line(int n)
 {
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(-4);
-	return (0);
+	int counter = 0;
+
+	while (n > 0 && counter < n)
+	{
+		_putchar('_');
+		counter++;
+	}
+	_putchar('\n');
 }
