f3d4daf4ca24c3b03e1367a194cfb101b01b1085
Bossdoli18 <oladeleife18@gmail.com>
2023-04-10 05:10:30 -0700

    update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,19 +1,20 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
+#ifndef MAIN_H
+#define MAIN_H
 
+/*
+ * File name: main.h
+ * 
+ * Desc: Header file containing all function prototypes
+ * 
+ */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-/* Reads a text file and prints its contents to the POSIX standard output. */
-ssize_t read_textfile(const char *file_path, size_t num_bytes);
-
-/* Creates a file and writes text content to it. */
-int create_file(const char *file_path, char *file_content);
-
-/* Appends text content to an existing file. */
-int append_text_to_file(const char *file_path, char *file_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
