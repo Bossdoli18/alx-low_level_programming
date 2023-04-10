f9722f822820edb54ffb190e6fc7bf42f1cee54a
Bossdoli18 <oladeleife18@gmail.com>
2023-04-10 15:56:15 -0700

    updated create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * create_file - creates a file
- * @filename: the name of the file to create
- * @text_content: the text content to write to the file
+ * read_textfile - function to read a text file to the POSIX stdout.
+ * @filename: points to the file name.
+ * @letters: number of characters the
+ *           function read and print.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 0 if filename is NULL or if function fail.
+ *         O/w - number of bytes the function reads and prints.
  */
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, w, len = 0;
+	ssize_t opn, rd, wt;
+	char *buffer;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (0);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len] != '\0')
-            len++;
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	opn = open(filename, O_RDONLY);
+	rd = read(opn, buffer, letters);
+	wt = write(STDOUT_FILENO, buffer, rd);
 
-    if (text_content != NULL)
-    {
-        w = write(fd, text_content, len);
-        if (w == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (opn == -1 || rd == -1 || wt == -1 || wt != rd)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    close(fd);
-    return (1);
+	free(buffer);
+	close(opn);
+
+	return (wt);
 }
-
