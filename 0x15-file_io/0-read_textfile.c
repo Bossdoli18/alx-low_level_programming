8d8aa40bc6df001f182a64365266cc214a47ea75
Bossdoli18 <oladeleife18@gmail.com>
2023-04-10 15:22:12 -0700

    edited the file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,49 +2,38 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the maximum number of characters to read and print
+ * read_textfile - function to read a text file to the POSIX stdout.
+ * @filename: points to the file name.
+ * @letters: number of characters the
+ *           function read and print.
  *
- * Return: If the function succeeds, the number of bytes read and printed.
- *         Otherwise, 0.
+ * Return: 0 if filename is NULL or if function fail.
+ *         O/w - number of bytes the function reads and prints.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t opn, rd, wt, total = 0;
-    char *buffer;
+	ssize_t opn, rd, wt;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    buffer = malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    opn = open(filename, O_RDONLY);
-    if (opn == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	opn = open(filename, O_RDONLY);
+	rd = read(opn, buffer, letters);
+	wt = write(STDOUT_FILENO, buffer, rd);
 
-    while ((rd = read(opn, buffer, letters)) > 0)
-    {
-        wt = write(STDOUT_FILENO, buffer, rd);
-        if (wt == -1 || wt != rd)
-        {
-            free(buffer);
-            close(opn);
-            return (0);
-        }
-        total += wt;
-    }
+	if (opn == -1 || rd == -1 || wt == -1 || wt != rd)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    free(buffer);
-    close(opn);
+	free(buffer);
+	close(opn);
 
-    if (rd == -1)
-        return (0);
-
-    return (total);
+	return (wt);
 }
